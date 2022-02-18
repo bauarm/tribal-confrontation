7ce1ddb54e46bebc1fc596289f1eca79117cbde1
Roman Babenko <oberbau@yahoo.com>
2022-02-18 23:51:35 +0300

    calendar

--- build/Animation.js
+++ build/Animation.js
@@ -6,9 +6,25 @@ const nextGameStep = (function () {
 export const timer = {
     tick: 0,
     curTick: 0,
+    day: 1,
+    mount: 3,
+    year: 1040,
 };
 function makeTime() {
+    console.log(`Day ${timer.day} mount ${timer.mount} year ${timer.year}`);
     timer.tick += 1;
+    if (timer.tick > 30) {
+        timer.day += 1;
+        timer.tick = 0;
+    }
+    if (timer.day > 30) {
+        timer.mount += 1;
+        timer.day = 1;
+    }
+    if (timer.mount > 12) {
+        timer.year += 1;
+        timer.mount = 1;
+    }
 }
 export function game() {
     nextGameStep(game);
--- src/Animation.ts
+++ src/Animation.ts
@@ -8,15 +8,34 @@ const nextGameStep = (function () {
 interface ITimer {
   tick:number;
   curTick:number;
+  day:number;
+  mount:number;
+  year:number;
 }
 
 export const timer:ITimer = {
   tick: 0,
   curTick: 0,
+  day: 1,
+  mount: 3,
+  year: 1040,
 };
 
 function makeTime() {
+  console.log(`Day ${timer.day} mount ${timer.mount} year ${timer.year}`);
   timer.tick += 1;
+  if (timer.tick > 30) {
+    timer.day += 1;
+    timer.tick = 0;
+  }
+  if (timer.day > 30) {
+    timer.mount += 1;
+    timer.day = 1;
+  }
+  if (timer.mount > 12) {
+    timer.year += 1;
+    timer.mount = 1;
+  }
 }
 
 export function game():void {
